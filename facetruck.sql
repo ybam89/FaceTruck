956eeb882e78196cb1dc7617b2c837da4df8d732
ybam89 <ybam89@gmail.com>
2024-11-27 21:02:36 -0600

    Add files via upload

--- facetruck.sql
+++ facetruck.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 21-11-2024 a las 21:25:34
+-- Tiempo de generación: 28-11-2024 a las 04:01:44
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -40,7 +40,7 @@ CREATE TABLE `empresas` (
 --
 
 INSERT INTO `empresas` (`id`, `usuario_id`, `pregunta_uno_empresas`, `pregunta_dos_empresas`, `pregunta_tres_empresas`) VALUES
-(1, 15, 'rrrrr', '3453453', '3');
+(1, 21, 'Empresa', 'dos', 'Tres');
 
 -- --------------------------------------------------------
 
@@ -61,7 +61,7 @@ CREATE TABLE `hombres_camion` (
 --
 
 INSERT INTO `hombres_camion` (`id`, `usuario_id`, `pregunta_uno_hombres_camion`, `pregunta_dos_hombres_camion`, `pregunta_tres_hombres_camion`) VALUES
-(1, 16, 'sadasdas', 'asdasdas', 'asdasd');
+(1, 20, 'HC_111', 'HC_222', 'HC_333');
 
 -- --------------------------------------------------------
 
@@ -82,7 +82,7 @@ CREATE TABLE `operadores` (
 --
 
 INSERT INTO `operadores` (`id`, `usuario_id`, `pregunta_uno_operadores`, `pregunta_dos_operadores`, `pregunta_tres_operadores`) VALUES
-(1, 17, 'Pregunta uno (1)', 'Pregunta dos (2)', 'Pregunta (3)');
+(1, 19, '1', '2', '3');
 
 -- --------------------------------------------------------
 
@@ -96,21 +96,18 @@ CREATE TABLE `usuarios` (
   `correo` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `tipo_usuario` enum('operador','hombreCamion','empresa') NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `foto_perfil` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `operador_id`, `correo`, `password`, `tipo_usuario`, `created_at`) VALUES
-(11, NULL, 'aaa@m.com', '$2y$10$m.3S7vR117IupQjysqRmNuZpFQyT5MbhgprAZRqLMt7TYe5jYAUK2', 'hombreCamion', '2024-11-20 04:54:25'),
-(12, NULL, 'ybam89@gmail.com', '$2y$10$vF/UF5pzA34OSIooMaZowuFWACHrFDZgUSmZqwgaG16PNPdy.UatK', 'empresa', '2024-11-20 05:05:41'),
-(13, NULL, 'a5546945370@gmail.com', '$2y$10$r.jNG4xalul6z2zljeFVT.D0iuTyto9k7VO8g1S0gLq6vIJ8BJJJe', 'operador', '2024-11-20 05:08:51'),
-(14, NULL, 'ggg@sms.com', '$2y$10$G537wjfV8E3Q24rXGTymOecXA/Gi6kJRiQY04SkurNLSjRpJ/P0me', 'hombreCamion', '2024-11-20 05:24:05'),
-(15, NULL, 'asasa@dsdaa.com', '$2y$10$MnHWuJlPROmWtSJDav4jn.72ImAC.f54nytQAXleOossS1oZlQVuK', 'empresa', '2024-11-20 05:35:53'),
-(16, NULL, 'hm@camion.com', '$2y$10$hfBwjhrkOHyra5iJR.Pis.w2u5t801d0kKJ4eo3t3SaXqn0pE1y8a', 'hombreCamion', '2024-11-20 18:23:12'),
-(17, NULL, 'jp@gmail.com', '$2y$10$3/hf8o9c.anFSHQnG2QPleN3aL4vm3uOswuDMgt.RE0wKhxPm1SB6', 'operador', '2024-11-21 17:53:08');
+INSERT INTO `usuarios` (`id`, `operador_id`, `correo`, `password`, `tipo_usuario`, `created_at`, `foto_perfil`) VALUES
+(19, NULL, 'operador@gmail.com', '$2y$10$rvGRCn5Z95WvG6g9dquMa.YCGqI5qCfSmCeZJoKbUsSIbVUrqp/IW', 'operador', '2024-11-22 14:46:00', 'uploads/67472bc6ba8a7_logoooo.png'),
+(20, NULL, 'hc@gmail.com', '$2y$10$sOwjUgbl8EKrYq9ZdDBhiempPhRuHVOhlgG7YtxaxFFfVYbbg0IF2', 'hombreCamion', '2024-11-22 14:56:30', 'uploads/6747aff6085e5_logoooo (2).png'),
+(21, NULL, 'em@gmail.com', '$2y$10$KU4lYfOt.RS6VlfaXUo.0uns8MP7jK0XYImHQV6vbSbvhOc.cCIvS', 'empresa', '2024-11-22 17:16:00', NULL);
 
 --
 -- Disparadores `usuarios`
@@ -184,7 +181,7 @@ ALTER TABLE `operadores`
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- Restricciones para tablas volcadas
